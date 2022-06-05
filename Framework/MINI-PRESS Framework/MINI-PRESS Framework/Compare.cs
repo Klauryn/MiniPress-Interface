d12239af3b3f3551801dff296e3833c8cc098359
Ozan Yürük <ozanyuruk@yahoo.com>
2022-06-05 11:27:13 +0300

    Chart Added
    A chart was used in compare page

--- Framework/MINI-PRESS Framework/MINI-PRESS Framework/Compare.cs
+++ Framework/MINI-PRESS Framework/MINI-PRESS Framework/Compare.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,16 +8,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace MINI_PRESS_Framework
 {
     public partial class Compare : Form
     {
         public static Compare instance;
+        public string path = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location).FullName; // return the application.exe current folder
+
         public Compare()
         {
             InitializeComponent();
             instance = this;
+        }
+
+        private void Compare_Load(object sender, EventArgs e)
+        {
+            
+        }
+        /*************************************\    EXCEL ROWS AND COLUMNS    /*******************************************/
+        public void Excel_RowAndColumn()
+        {
+            string fileName = Path.Combine(path, "Reference\\Açık Tip 7_5 Ton_3.csv"); 
+            Excel _excel = new Excel(fileName, 1);
+            Tuple<int, int> _tuple = _excel.RowsAndColumns();
+            _excel.Save();
+            _excel.Close();
+        }
+        /*************************************\    EXCEL OPEN FILE    /*******************************************/
+        public void Excel_OpenFile(string data, string seriesName)
+        {
+            chart1.Series.Add(seriesName).ChartType = SeriesChartType.Point;
+            chart1.Series.FindByName(seriesName).MarkerSize = 3;
+
+            chart1.ChartAreas[0].AxisY.Maximum = 6500;
+            chart1.ChartAreas[0].AxisX.Minimum = 230;
+
+            chart1.Titles.Add("Line Chart Example");
+
+            string fileName = Path.Combine(path, data);
+            string[] csvLines = File.ReadAllLines(fileName);
+            var firstNames = new List<double>();
+            var secondNames = new List<double>();
+
+            for (int i = 1; i < csvLines.Length; i++)
+            {
+                string[] rowData = csvLines[i].Split(';');
+                firstNames.Add(double.Parse(rowData[0]));
+                secondNames.Add(double.Parse(rowData[1]));
+            }
+
+            Excel _excel = new Excel(fileName, 1);
+
+            for (int i = 1; i < csvLines.Length/*_tuple.Item1*/; i++)
+            {
+                chart1.Series[seriesName].Points.AddXY(firstNames[i - 1], secondNames[i - 1]);
+            }
+            _excel.Save();
+            _excel.Close();
+        }
+
+        /*************************************\    EXCEL WRİTE DATA    /*******************************************/
+        public void Excel_WriteData()
+        {
+            string fileName = Path.Combine(path, "Excel\\Created.xlsx");
+            Excel _excel = new Excel(fileName, 1);
+            _excel.WritetoCell(0, 0, "kuçu");
+            _excel.Save();
+
+            /*fileName = Path.Combine(path, "Excel\\WriteDataTest.xlsx");
+            _excel.SaveAs(fileName);*/
+            _excel.Close();
+        }
+
+        /*************************************\    EXCEL CREATE FILE    /*******************************************/
+        public void Excel_CreateFile()
+        {
+            Excel _excel = new Excel();
+            _excel.CreateNewFile();
+            string fileName = Path.Combine(path, "Excel\\Created.xlsx");
+            _excel.SaveAs(fileName);
+            _excel.Close();
+        }
+        /*************************************\    CALL EXCEL TO CHART    /*******************************************/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel_OpenFile("Reference\\Açık Tip 7_5 Ton_3.csv", "B1");
+            Excel_OpenFile("Reference\\Açık Tip 7_5 Ton_2.csv", "B2");
         }
     }
 }
---
+++ Framework/MINI-PRESS Framework/MINI-PRESS Framework/Excel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace MINI_PRESS_Framework
+{
+    class Excel
+    {
+        /***********************************\   CREATE EXCEL APPLICATION    /*****************************************/
+        string path = "";
+        _Application excel = new _Excel.Application();  //Excel Application oluşturuldu.
+        Workbook wb;                                    //Workbook oluşturuldu.
+        Worksheet ws;                                   //Worksheet oluşturuldu.
+        public Excel()
+        {
+            ;
+        }
+
+        /********************************\   PATH AND SHEET EXCEL CONSTRUCTOR   /*************************************/
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = excel.Worksheets[Sheet];
+        }
+
+        /************************************\    CALL ROWS AND COLUMNS   /*******************************************/
+        public Tuple<int, int> RowsAndColumns()
+        {
+            _Excel.Range _Range = ws.UsedRange;
+            int nRows = _Range.Rows.Count;
+            int nCols = _Range.Columns.Count;
+            return new Tuple<int, int>(nRows, nCols);
+        }
+
+        /*************************************\     READ CELL    /*******************************************/
+        public object ReadCell(int i, int j)
+        {
+
+            if (ws.Cells[i, j].Value2 != null)
+            {
+                object sendData = ws.Cells[i, j].Value2;
+                return sendData;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        /*************************************\    WRİTE CELL    /*******************************************/
+        public void WritetoCell(int i, int j, string value)
+        {
+            i++; j++;
+            ws.Cells[i, j].Value2 = value;
+        }
+
+        /*************************************\    SAVE    /*******************************************/
+        public void Save()
+        {
+            wb.Save();
+        }
+
+        /*************************************\    SAVE AS    /*******************************************/
+        public void SaveAs(string path)
+        {
+            wb.SaveAs(path);
+        }
+
+        /*************************************\    CLOSE    /*******************************************/
+        public void Close()
+        {
+            wb.Close();
+        }
+
+        /**********************************\    CREATE NEW FILE    /*******************************************/
+        public void CreateNewFile()
+        {
+            this.wb = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+        }
+
+        /*public int deneme()
+        {
+            _Excel.Range _Range = ws.UsedRange;
+            int x = _Range.ReadingOrder;
+            return x;
+        }*/
+    }
+}
+
